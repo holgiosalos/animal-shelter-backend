ac145003d2bf5303b47a3220009a56c41fc0e3be
Yesid <yesid.lopez@correo.icesi.edu.co>
2021-03-07 13:09:55 -0500

    feature: delete and update animals and update status codes

--- src/index.js
+++ src/index.js
@@ -34,9 +34,35 @@ app.get('/animals', (req, res) => {
 
 app.post('/animal', (req, res) => {
     const animal = req.body;
-    if (!animals.some(e => e.name === animal.name)) {
+    if (!animals.some(e => e.name.toUpperCase() === animal.name.toUpperCase())) {
         animals.push(animal);
+        res.status(201).send(animal);
+    } else {
+        res.status(409).json({ message: 'The animal has already been created' });
     }
+});
 
-    res.send(animals);
+app.put('/animal/:name', (req, res) => {
+    const { name } = req.params;
+    const { breed, gender, isVaccinated, vaccines } = req.body;
+    const filteredAnimal = animals.filter(animal => animal.name.toUpperCase() === name.toUpperCase());
+    if (filteredAnimal.length > 0) {
+        filteredAnimal[0].breed = breed
+        filteredAnimal[0].gender = gender
+        filteredAnimal[0].isVaccinated = isVaccinated
+        filteredAnimal[0].vaccines = vaccines
+        animals.splice(animals.findIndex(animal => animal.name.toUpperCase() === name.toUpperCase()), 1, filteredAnimal[0]);
+        res.status(200).send(animals);
+    } else
+        res.status(404).json({ message: `Animal with the name ${name} not found` });
+});
+
+app.delete('/animal/:name', (req, res) => {
+    const { name } = req.params;
+    const filteredAnimal = animals.filter(animal => animal.name.toUpperCase() === name.toUpperCase());
+    if (filteredAnimal.length > 0) {
+        animals.splice(animals.findIndex(animal => animal.name.toUpperCase() === name.toUpperCase()), 1);
+        res.status(200).json({ message: `Animal deleted successfully` });
+    } else
+        res.status(404).json({ message: `Animal with the name ${name} not found` });
 });
