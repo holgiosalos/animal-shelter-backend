c504d48271ab0a558728148c10838391d69a3820
Holmes Salazar <hgsosorio@gmail.com>
2022-03-31 16:45:14 -0500

    Updating tests to run in localhost

--- test/specs/deleteAnimal.spec.js
+++ test/specs/deleteAnimal.spec.js
@@ -2,6 +2,7 @@ const expect = require('chai').expect;
 const axios = require('axios');
 const faker = require('faker').random;
 
+const baseUrl = 'http://localhost:8080';
 let createdAnimal;
 let response;
 describe('Given a created animal', () => {
@@ -18,13 +19,13 @@ describe('Given a created animal', () => {
             ]
         };
 
-        createdAnimal = (await axios.post('https://animal-shelter-back.herokuapp.com/animals', animal)).data;
+        createdAnimal = (await axios.post(`${baseUrl}/animals`, animal)).data;
 
     });
 
     describe('When the user wants to delete the animal', () => {
         before(async () => {
-            response = await axios.delete(`https://animal-shelter-back.herokuapp.com/animals/${createdAnimal.name}`)
+            response = await axios.delete(`${baseUrl}/animals/${createdAnimal.name}`)
         });
 
         it('Then should have an OK status code', () => {
